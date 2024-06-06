6a3e11b937ea4a34d7bbd204036b315f324a7c7e
Kyle Greenidge <kylegreen149@gmail.com>
2024-06-05 22:04:57 -0400

    Done

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction(name = "User") {
+    return `Hi, my name is ${name}.`
+}
+introduction("Aki")
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguage("Aki", "Ember.js")
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
